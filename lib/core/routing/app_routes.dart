dacccb528b863291fba89d368e5cb49ed5ebebdf
= <=>
2026-08-14 15:22:09 +0300

    chore: set up project architecture and dependencies

---
+++ lib/core/routing/app_routes.dart
@@ -0,0 +1,9 @@
+abstract final class AppRoutes {
+  static const splash = '/splash';
+  static const login = '/login';
+  static const home = '/home';
+  static const productDetails = '/products/:productId';
+  static const cart = '/cart';
+  static const checkout = '/checkout';
+  static const profile = '/profile';
+}
